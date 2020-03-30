ebffa0e07d3bcc0ddf6ed5990d031c839cf05fff
Donghyeon Kim <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-03-30 19:45:25 +0900

    docs : ADD comments and RENAME
    마찬가지로 주석을 추가하고 이름 수정

--- _summary_python_code/4/02-1-zip응용dict변환-1.py
+++ _summary_python_code/4/02-1-zip응용dict변환-1.py
@@ -2,7 +2,12 @@
 keys = [i for i in range(1, 11)]
 # values = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]
 values = [2**i for i in range(1, 11)]
-myzip = zip(keys,values)
+myzip = zip(keys, values)
 mydict = dict(myzip)
 
 print(mydict)
+
+"""zip함수 dict응용:
+zip의 인자가 2개라면, 2개씩 묶어주기 때문에, dict()함수를 통해
+딕셔너리 자료형을 쉽게 변환할 수 있다.
+"""
