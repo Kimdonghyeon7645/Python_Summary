86bab5391cd46fe076bb8c0d985a07cece1de4a2
Donghyeon Kim <48408417+Kimdonghyeon7645@users.noreply.github.com>
2020-04-10 18:07:14 +0900

    docs : delete old files

--- _summary_python_code/5/01.py
+++
@@ -1,9 +0,0 @@
-import random as r
-
-r_list = [r.randint(1, 100) for i in range(10)]    # 랜덤 기본 리스트
-r_list.append(50)    # 맨 뒤에 50 추가
-r_list.sort()    # 정렬
-del r_list[::2]    # 짝수 자리의 요소 제거
-r_list2 = [r.randint(100, 200) for i in range(5)]    # 새로운 리스트
-f_list = r_list + r_list2    # 두 리스트 합체
-print(f_list)
